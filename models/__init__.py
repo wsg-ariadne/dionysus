700ceb2560a608de57aa3d6720cc42878df9add2
Jared Dantis <jareddantis@gmail.com>
2023-03-20 19:09:21 +0800

    models: Enumerate recognized design types

---
+++ models/__init__.py
@@ -0,0 +1,7 @@
+# Recognized types of deceptive design
+VALID_DESIGN_TYPES = [
+    'unclear_language',
+    'weighted_options',
+    'prefilled_options',
+    'other'                  # When this is selected, make sure to check the custom_deceptive_design_type field
+]
