d4de807c4528909de604c257ca0ded78f70699f4
Jared Dantis <jareddantis@gmail.com>
2023-04-05 23:40:26 +0800

    models: Add created column

--- models/page.py
+++ models/page.py
@@ -1,9 +1,10 @@
 from database import db
-
+from datetime import datetime
 
 class Page(db.Model):
     __tablename__ = 'pages'
     id = db.Column(db.BigInteger, primary_key=True, nullable=False)
+    created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     site_id = db.Column(db.BigInteger, db.ForeignKey('sites.id'), nullable=False)
     path = db.Column(db.String(255), nullable=False)
     deceptive_design_types = db.Column(db.String(255), nullable=False)
--- models/report.py
+++ models/report.py
@@ -7,9 +7,10 @@ from uuid import uuid4
 class Report(db.Model):
     __tablename__ = 'reports'
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
+    created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     domain = db.Column(db.String(255), nullable=False)
     path = db.Column(db.String(255), nullable=False)
     deceptive_design_type = db.Column(db.String(255), nullable=False)
     is_custom_type = db.Column(db.Boolean, nullable=False)
     num_reports = db.Column(db.Integer, nullable=False)
-    last_report_timestamp = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    last_report_timestamp = db.Column(db.DateTime, nullable=False)
